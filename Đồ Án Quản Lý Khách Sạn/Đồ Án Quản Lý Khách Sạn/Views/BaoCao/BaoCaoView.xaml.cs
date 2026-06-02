480d8edf6ef88b1cfa4d7059d1c44b5211e94c77
UIT-23521602 <23521602@gm.uit.edu.vn>
2026-06-02 21:08:34 +0700

    dieu chinh cho module BaoCao

--- Đồ Án Quản Lý Khách Sạn/Đồ Án Quản Lý Khách Sạn/Views/BaoCao/BaoCaoView.xaml.cs
+++ Đồ Án Quản Lý Khách Sạn/Đồ Án Quản Lý Khách Sạn/Views/BaoCao/BaoCaoView.xaml.cs
@@ -4,10 +4,13 @@ using Đồ_Án_Quản_Lý_Khách_Sạn.ViewModels;
 
 namespace Đồ_Án_Quản_Lý_Khách_Sạn.Views.BaoCao
 {
+    /// Lớp code-behind cho màn hình Báo cáo doanh thu và Thống kê công suất phòng
     public partial class BaoCaoView : UserControl
     {
+        /// Khởi tạo các thành phần giao diện của màn hình Báo cáo
         public BaoCaoView() => InitializeComponent();
 
+        /// Mở hộp thoại Thống kê doanh thu theo Loại phòng của năm đang chọn
         private void BtnThongKeLoaiPhong_Click(object sender, RoutedEventArgs e)
         {
             var vm = (BaoCaoViewModel)DataContext;
@@ -15,6 +18,7 @@ namespace Đồ_Án_Quản_Lý_Khách_Sạn.Views.BaoCao
             dlg.ShowDialog();
         }
 
+        /// Mở hộp thoại Thống kê số lượt thuê/công suất sử dụng từng phòng cụ thể
         private void BtnThongKePhong_Click(object sender, RoutedEventArgs e)
         {
             var vm = (BaoCaoViewModel)DataContext;
